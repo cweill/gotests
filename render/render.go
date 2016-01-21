83394405dc554953d336f1972e36929bc5a9dca2
Charles Weill <cew225@cornell.edu>
2016-01-20 20:20:48 -0500

    Fix bug where render could not find templates directory.

--- render/render.go
+++ render/render.go
@@ -2,12 +2,19 @@ package render
 
 import (
 	"io"
+	"path"
+	"runtime"
 	"text/template"
 
 	"github.com/cweill/gotests/models"
 )
 
-var tmpls = template.Must(template.ParseGlob("render/templates/*.tmpl"))
+var tmpls *template.Template
+
+func init() {
+	_, filename, _, _ := runtime.Caller(1)
+	tmpls = template.Must(template.ParseGlob(path.Join(path.Dir(filename), "templates/*.tmpl")))
+}
 
 func Header(w io.Writer, info *models.Info) error {
 	return tmpls.ExecuteTemplate(w, "header", info)
