6575dae531dc81c30bc39e235fee0e5fd2381cf0
Charles Weill <cew225@cornell.edu>
2016-01-22 00:26:10 -0500

    Add test for functions handling channels.

--- gotests_test.go
+++ gotests_test.go
@@ -542,6 +542,31 @@ func TestFoo22(t *testing.T) {
 }
 `,
 		}, {
+			name:    "Function w/ channel parameter and result",
+			srcPath: `testfiles/test023.go`,
+			want: `package testfiles
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestFoo23(t *testing.T) {
+	tests := []struct {
+		name string
+		ch   chan bool
+		want chan string
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		if got := Foo23(tt.ch); !reflect.DeepEqual(got, tt.want) {
+			t.Errorf("%v. Foo23() = %v, want %v", tt.name, got, tt.want)
+		}
+	}
+}
+`,
+		}, {
 			name:    "Multiple functions",
 			srcPath: `testfiles/test100.go`,
 			want: `package testfiles
---
+++ testfiles/test023.go
@@ -0,0 +1,3 @@
+package testfiles
+
+func Foo23(ch chan bool) chan string { return make(chan string) }
