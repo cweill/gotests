cfccbcf23160476b41fcf548d69d80f7521685a3
Charles Weill <cew225@cornell.edu>
2016-01-21 19:40:30 -0500

    Add example incomplete test file.

--- testfiles/test100.go
+++ testfiles/test100.go
@@ -1,7 +1,14 @@
 package testfiles
 
+import "errors"
+
 func Foo100(strs []string) ([]*Bar, error) { return nil, nil }
 
-func (b *Bar) Bar100(i interface{}) error { return nil }
+func (b *Bar) Bar100(i interface{}) error {
+	if i == nil {
+		return errors.New("i is nil")
+	}
+	return nil
+}
 
-func baz100(f *float64) float64 { return 0 }
+func baz100(f *float64) float64 { return *f }
---
+++ testfiles/test100_test.go
@@ -0,0 +1,42 @@
+package testfiles
+
+import "testing"
+
+func TestBar100(t *testing.T) {
+	tests := []struct {
+		name    string
+		b       *Bar
+		i       interface{}
+		wantErr bool
+	}{
+		{
+			name:    "Basic test",
+			b:       &Bar{},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		if err := tt.b.Bar100(tt.i); (err != nil) != tt.wantErr {
+			t.Errorf("%v. Bar100() error = %v, wantErr: %v", tt.name, err, tt.wantErr)
+		}
+	}
+}
+
+func TestBaz100(t *testing.T) {
+	tests := []struct {
+		name string
+		f    *float64
+		want float64
+	}{
+		{
+			name: "Basic test",
+			f:    func() *float64 { var x float64 = 64; return &x }(),
+			want: 64,
+		},
+	}
+	for _, tt := range tests {
+		if got := baz100(tt.f); got != tt.want {
+			t.Errorf("%v. baz100() = %v, want %v", tt.name, got, tt.want)
+		}
+	}
+}
