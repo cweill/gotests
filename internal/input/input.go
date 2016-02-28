c000ce1d6a33e42304059b03f0ee2e49f68b9f8b
Charles Weill <weill@google.com>
2016-02-28 14:50:12 -0500

    Split input.Files into smaller functions.

--- internal/input/input.go
+++ internal/input/input.go
@@ -1,46 +1,46 @@
 package input
 
 import (
-	"errors"
 	"fmt"
 	"path/filepath"
 
 	"github.com/cweill/gotests/internal/models"
 )
 
-var ErrNoFilesFound = errors.New("no files found")
-
 // Returns all the Golang files for the given path. Ignores hidden files.
 func Files(srcPath string) ([]models.Path, error) {
-	var srcPaths []models.Path
 	srcPath, err := filepath.Abs(srcPath)
 	if err != nil {
 		return nil, fmt.Errorf("filepath.Abs: %v\n", err)
 	}
 	if filepath.Ext(srcPath) == "" {
-		ps, err := filepath.Glob(srcPath + "/*.go")
-		if err != nil {
-			return nil, fmt.Errorf("filepath.Glob: %v\n", err)
-		}
-		for _, p := range ps {
-			if isHiddenFile(p) {
-				continue
-			}
-			src := models.Path(p)
-			if !src.IsTestPath() {
-				srcPaths = append(srcPaths, src)
-			}
-		}
-		return srcPaths, nil
+		return dirFiles(srcPath)
 	}
-	if filepath.Ext(srcPath) == ".go" && !isHiddenFile(srcPath) {
-		src := models.Path(srcPath)
-		if !src.IsTestPath() {
-			srcPaths = append(srcPaths, src)
-		}
-		return srcPaths, nil
+	return file(srcPath)
+}
+
+func dirFiles(srcPath string) ([]models.Path, error) {
+	ps, err := filepath.Glob(srcPath + "/*.go")
+	if err != nil {
+		return nil, fmt.Errorf("filepath.Glob: %v\n", err)
 	}
-	return nil, fmt.Errorf("no files found at %v", srcPath)
+	var srcPaths []models.Path
+	for _, p := range ps {
+		src := models.Path(p)
+		if isHiddenFile(p) || src.IsTestPath() {
+			continue
+		}
+		srcPaths = append(srcPaths, src)
+	}
+	return srcPaths, nil
+}
+
+func file(srcPath string) ([]models.Path, error) {
+	src := models.Path(srcPath)
+	if filepath.Ext(srcPath) != ".go" || isHiddenFile(srcPath) || src.IsTestPath() {
+		return nil, fmt.Errorf("no Go source files found at %v", srcPath)
+	}
+	return []models.Path{src}, nil
 }
 
 func isHiddenFile(path string) bool {
