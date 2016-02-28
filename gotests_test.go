b28417b9a179932a16af93cb0e027ca20dad9045
Charles Weill <weill@google.com>
2016-02-28 16:18:44 -0500

    Add test for undefined types in source file.

--- gotests_test.go
+++ gotests_test.go
@@ -2087,6 +2087,38 @@ func TestNot(t *testing.T) {
 	}
 }
 `,
+		}, {
+			name:    "Undefined types",
+			srcPath: `testdata/undefinedtypes/undefined.go`,
+			want: `package undefinedtypes
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestUndefinedDo(t *testing.T) {
+	tests := []struct {
+		name    string
+		u       *Undefined
+		es      Something
+		want    *Unknown
+		wantErr bool
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		got, err := tt.u.Do(tt.es)
+		if (err != nil) != tt.wantErr {
+			t.Errorf("%q. Undefined.Do() error = %v, wantErr %v", tt.name, err, tt.wantErr)
+			continue
+		}
+		if !reflect.DeepEqual(got, tt.want) {
+			t.Errorf("%q. Undefined.Do() = %v, want %v", tt.name, got, tt.want)
+		}
+	}
+}
+`,
 		},
 	}
 	for _, tt := range tests {
