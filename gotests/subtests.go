802f0b3bd0da71a36899c7297d7cf015e6c4c543
Jamie Stackhouse <contin673@gmail.com>
2016-11-07 10:04:29 -0400

    Forgot to updated variable name in flag.

--- gotests/subtests.go
+++ gotests/subtests.go
@@ -5,5 +5,5 @@ package main
 import "flag"
 
 func init() {
-	flag.BoolVar(&subtests, "nosubtests", false, "disable generating tests using the Go 1.7 subtests feature")
+	flag.BoolVar(&nosubtests, "nosubtests", false, "disable generating tests using the Go 1.7 subtests feature")
 }
